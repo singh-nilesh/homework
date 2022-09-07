efad13ba3a4a9576cfdfbe908487d6b387f4f66a
Nilesh Singh <112500224+singh-nilesh@users.noreply.github.com>
2022-09-07 09:05:04 +0530

    odd_even

--- homework/python_06_09_22/06-09-22(Q10).py
+++
@@ -1,6 +0,0 @@
-# this program check wether the number is even or odd
-num= int(input("Enter the number"))
-if num%2==0:
-    print("The number %d is even"%num)
-else:
-    print("The number %d is odd"%num)
