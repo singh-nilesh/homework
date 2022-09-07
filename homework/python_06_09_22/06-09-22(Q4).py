019d3d09c81d6ab4c8718f0af9241f62bacaea65
Nilesh Singh <112500224+singh-nilesh@users.noreply.github.com>
2022-09-07 08:25:31 +0530

    vowel_consonent

---
+++ homework/python_06_09_22/06-09-22(Q4).py
@@ -0,0 +1,16 @@
+#This program checks either the charecter entered is vowel or consonent
+
+char = input("Enter any one alphabetic letter:")
+
+if char=='a':
+    print("%s is a vowel"%char)
+elif char=='e':
+    print("%s is a vowel"%char)
+elif char=='i':
+    print("%s is a vowel"%char)
+elif char=='o':
+    print("%s is a vowel"%char)
+elif char=='u':
+    print("%s is a vowel"%char)
+else:
+    print("%s is a consonent"%char)
