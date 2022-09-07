91e86a0941c4514a832455b2943dd48ae1137dc5
Nilesh Singh <112500224+singh-nilesh@users.noreply.github.com>
2022-09-07 09:11:23 +0530

    leap_year

---
+++ homework/python_06_09_22/06-09-22(Q9).py
@@ -0,0 +1,7 @@
+# This program tels if thr year entered is leap year or not
+
+year= int(input("Enter the yeat:"))
+if year%4==0:
+    print(" The year %d is a leap year"%year)
+else:
+    print(" The tear %d is not a leap year"%year)
