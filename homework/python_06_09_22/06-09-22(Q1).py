1814a15778f179b501a77de5d168ae21cb7591b5
Nilesh Singh <112500224+singh-nilesh@users.noreply.github.com>
2022-09-11 18:25:24 +0530

    save

--- homework/python_06_09_22/06-09-22(Q1).py
+++ homework/python_06_09_22/06-09-22(Q1).py
@@ -1,8 +1,7 @@
 # this program reads value of n and checks if the numbers is zero or non-zero value.
-import sys
+
 no = float(input("enter a number:")) 
 if no==0:
     print("The number %d is a zero value"%no)
 else:
     print("The number %d is a non zero value"%no)
-sys.exit()
