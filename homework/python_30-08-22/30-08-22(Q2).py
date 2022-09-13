5ee6b358ff6182e81df5e4d8120044f97d838713
Nilesh Singh <112500224+singh-nilesh@users.noreply.github.com>
2022-09-13 21:44:56 +0530

    save

--- homework/python_30-08-22/30-08-22(Q2).py
+++
@@ -1,7 +0,0 @@
-# This a program to display z patter using 6 digit numbers
-no_1='345609'
-no_2='584759'
-no_3='359686'
-no_4='873069'
-no_5='670245'
-print(no_1+'\n\t'+no_2+'\n\t\t'+no_3+'\n\t'+no_4+'\n'+no_5)
